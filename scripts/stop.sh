462b93b3d5af58b29e3c7b9c3cf5baa83d894624
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2022-02-02 13:32:42 -0300

    Typo

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,6 +2,6 @@
 
 echo "Stopping the application"
 
-docker container stop "$(docker ps --filter name=store-manager-api* -aq)"
-docker container prune -filter
+docker container stop "$(docker ps --filter name=store-manager-api -aq)"
+docker container prune -f
 docker image prune -af
